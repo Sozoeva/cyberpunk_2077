4c80c9a41adf2384fedf1b24c4075a8fa899ea6d
Sozoeva <sozoeva.alina@gmail.com>
2023-09-28 09:30:26 +0600

    made header and main

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
+import styles from "./Header.module.scss";
+import logo from "../../assers/images/logo.png";
 
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <img src={logo} alt="logo" />
+        <div className={styles.links}>
+        <div>
+          <a className={styles.link} href="">Cyberpunk 2077</a>
+        </div>
+        <div>
+          <a className={styles.link} href="">Призрачная свовода</a>
+        </div>
+        <div>
+          <a className={styles.link} href="">Новости</a>
+        </div>
+        <div>
+          <a className={styles.link} href="">Смотреть</a>
+        </div>
+        <div>
+          <a className={styles.link} href="">Купить</a>
+        </div>
+        </div>
+
+      </nav>
+    </header>
+  );
+};
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import styles from "./Main.module.scss"
+import React from "react";
+import styles from "./Main.module.scss";
 
 export const Main = () => {
   return (
-    <div>Main</div>
-  )
-}
+    <main className={styles.main}>
+      <video autoPlay loop muted>
+        <source
+          src="https://cdn-l-cyberpunk.cdprojektred.com/video/EP1_ProjectAce_clean_16x9_15s-av1.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <div className="container">
+        <div className={styles.main_about}>
+          <h2>"Призрачная свобода" уже доступна</h2>
+          <div className={styles.main_btns}>
+          <button>Подробнее</button>
+          <button>Трейлер</button>
+          </div>
+          
+        </div>
+      </div>
+    </main>
+  );
+};
